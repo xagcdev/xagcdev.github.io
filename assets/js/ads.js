84ff6679ca9d3018338416149243e27dd8fb102e
XAGC | Web Developer <107314908+xagcdev@users.noreply.github.com>
2022-06-25 11:33:41 +0700

    Create ads.js

---
+++ assets/js/ads.js
@@ -0,0 +1,7 @@
+// Adsterra
+(function()
+ let script = document.createElement('script');
+ script.type = 'text/javascript';
+ script.src = '//pl17383636.profitablecpmgate.com/18/a4/38/18a438b4c9c384a6f2d2033dccccef7b.js';
+ document.body.append(script);
+)();
