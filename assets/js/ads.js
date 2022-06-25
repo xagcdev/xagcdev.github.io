cd95f06320218d326ba2ad117c6b850086ecd04c
XAGC | Web Developer <107314908+xagcdev@users.noreply.github.com>
2022-06-25 11:37:12 +0700

    Update ads.js

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -1,7 +1,7 @@
 // Adsterra
 (function()
  let script = document.createElement('script');
- script.type = 'text/javascript';
- script.src = '//pl17383636.profitablecpmgate.com/18/a4/38/18a438b4c9c384a6f2d2033dccccef7b.js';
+ script.setAttribute('src', 'text/javascript');
+ script.setAttribute('src', '//pl17086594.profitablecpmgate.com/b0/e0/f7/b0e0f7aef3540e5e4e5fbe40b4d9e002.js');
  document.body.append(script);
 )();
